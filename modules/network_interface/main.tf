2fcd49131a937fcd590b667f7caa5be3ea0e1de5
Chandudixitt <yelheriv@gmail.com>
2024-07-10 14:07:48 +0530

    Update main.tf

--- modules/network_interface/main.tf
+++ modules/network_interface/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "main" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = var.public_ip_id
   }
 
@@ -17,4 +17,3 @@ resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = var.nsg_id
 }
-
