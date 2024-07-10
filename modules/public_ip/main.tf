33cfbabfc180d04c1c38ba52f598721f0171b797
Chandudixitt <yelheriv@gmail.com>
2024-07-10 14:12:20 +0530

    Update main.tf

--- modules/public_ip/main.tf
+++ modules/public_ip/main.tf
@@ -2,7 +2,6 @@ resource "azurerm_public_ip" "main" {
   name                = var.public_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
   tags                = var.tags
 }
-
